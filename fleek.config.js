a4cf297c1cde94b475b33e00186cec72458cddd1
Felipe Mendes <zo.fmendes@gmail.com>
2023-12-13 11:40:55 -0300

    chore: add fleek github ci

---
+++ fleek.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@fleekxyz/cli').FleekConfig} */
+module.exports = {
+  "sites": [
+    {
+      "slug": "scrawny-death-wailing",
+      "distDir": "dist",
+      "buildCommand": "yarn && yarn build"
+    }
+  ]
+};
